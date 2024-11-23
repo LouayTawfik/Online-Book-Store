d3715419f91c1d03a6ca4f1cc47fa5ad82dd228f
Louay Tawfik <codebreaker007@outlook.com>
2024-11-23 17:40:17 +0200

    remove unnecessary lines of code

--- dummy_data.py
+++ dummy_data.py
@@ -18,24 +18,4 @@ def seed_books(n):
             image=f'books/{images[random.randint(0, 6)]}'
         )
     print(f'Seed {n} books successfully!')
-
-# def seed_product(n):
-#     fake = Faker()
-#     images = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpg', '6.jpg', '7.jpeg', '8.jpg', '9.jpg']
-#     flags = ['New', 'Sale', 'Feature']
-#     for _ in range(n):
-#         Product.objects.create(
-#             name=fake.name(),
-#             image=f'products/{images[random.randint(0, 8)]}',
-#             flag= flags[random.randint(0, 2)],
-#             price=round(random.uniform(20.99, 99.99), 2),
-#             sku=random.randint(1000, 10000000),
-#             subtitle=fake.text(max_nb_chars=250),
-#             description=fake.text(max_nb_chars=2000),
-#             quantity=random.randint(0, 30),
-#             brand=Brand.objects.get(id=random.randint(1, 5))
-#         )
-#     print(f'Seed {n} products successfully!')
-
-# seed_brand(5)
 seed_books(5)
